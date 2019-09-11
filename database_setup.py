77fdaec4e9c379fa2b1ee0add794c74a655866de
Anthony Ryan <=>
2019-09-11 13:59:36 -0400

    updated databasesetup script to connect to postgres

--- database_setup.py
+++ database_setup.py
@@ -4,9 +4,41 @@ from sqlalchemy import Column, ForeignKey, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+import psycopg2
+from configparser import ConfigParser
 
 Base = declarative_base()
-
+class MonthlyBills():
+    
+    def __init__(self):
+        self.filename ='/home/trock/Desktop/database.ini'
+        self.section = 'postgresql'
+        
+        
+    def config(self):
+        parser = ConfigParser()
+        parser.read(self.filename)
+        db = {}
+        if parser.has_section(self.section):
+            params = parser.items(self.section)
+            for param in params:
+                db[param[0]] = param[1]
+        else:
+            raise Exception('Section {0} not found in the {1} file'.format(section,filename) )
+        return db
+    def connect(self):
+        conn = None
+        try:
+            params = self.config()
+            print(params)
+            conn = psycopg2.connect(**params)
+            enginestart = 'postgresql+psycopg2://' + params['user'] + ':' + params['password'] + '@' + params['host']  + ':5432/' + params['database']
+            print(enginestart)
+            engine = create_engine(enginestart)
+            return engine
+        except(Exception) as error:
+            print('there is an error')
+            print(error)
 
 class MonthlyBill(Base):
     __tablename__ = 'monthlyBills'
@@ -40,5 +72,10 @@ class BankBalance(Base):
     date = Column(DateTime)
     UserID = Column(String(128), nullable = False)
 
-engine = create_engine('sqlite:///TRBills.db')
-Base.metadata.create_all(engine)
+if __name__ == '__main__':    
+    mb = MonthlyBills()
+    engine = mb.connect()
+#     engine = create_engine('sqlite:///TRBills.db')
+    Base.metadata.create_all(engine)
+# engine = create_engine('sqlite:///TRBills.db')
+    Base.metadata.create_all(engine)
